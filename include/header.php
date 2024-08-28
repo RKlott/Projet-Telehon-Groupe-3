9ae739178292fadb97845db5411f9708fd601d74
William <proklott@gmail.com>
2024-08-28 15:17:42 +0200

    Mise à jour du fichier des couleurs dans sass et des fichiers

--- include/header.php
+++ include/header.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title ?></title>
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="../public/css/index.css">
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
 <body>
-    <div class="w-150 bg-red-300">
-        Ceci est un test front
-    </div>
+    <header>
+        <img src="../public/images/logo.png" alt="logo du site">
+    </header>
