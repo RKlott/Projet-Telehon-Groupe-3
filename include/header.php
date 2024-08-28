1329c2593cc486ee449722f13288d9f7427eb04c
William <proklott@gmail.com>
2024-08-28 12:10:48 +0200

    Ajout de variables de couleurs

--- include/header.php
+++ include/header.php
@@ -6,6 +6,7 @@
     <title><?= $page_title ?></title>
     <link rel="stylesheet" href="./css/index.css">
     <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body>
     <div class="w-150 bg-red-300">
