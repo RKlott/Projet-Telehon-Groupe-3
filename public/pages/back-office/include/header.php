2961092da2990a289fbd8f90e126dd6026445725
William <proklott@gmail.com>
2024-08-29 10:42:37 +0200

    Modification du header pour rendre les liens de la navbar fonctionnels

--- public/pages/back-office/include/header.php
+++ public/pages/back-office/include/header.php
@@ -8,6 +8,19 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="w-150 bg-blue-300">
-        Ceci est un test back
-    </div>
+<header class="">
+        <img id="logo-desktop" src=<?= $desktop_logo_path?> class="" alt="logo du site">
+        
+<!--//TODO Importer un logo de la version tablette avec les bonnes dimensions (ce logo là est mal dimensionné)-->
+        <img id="logo-tablet" src=<?= $tablet_logo_path?> class="" alt="logo du site en version tablette">
+        <nav class="">
+            <ul class="">
+                <li class="backoffice_navbar_link"><a href=<?= $collectes_page_path?>>Collectes</a></li>
+                <li class="backoffice_navbar_link"><a href=<?= $blog_page_path?>>Blog</a></li>
+                <li class="backoffice_navbar_link"><a href=<?= $partenaires_page_path?>>Partenaires</a></li>
+                <li class="backoffice_navbar_link"><a href=<?= $evenements_page_path?>>Evenements</a></li>
+                <li class="backoffice_navbar_link"><a href=<?= $utilisateurs_page_path?>>Utilisateurs</a></li>
+                <li class="backoffice_navbar_link" id="navbar_last_element"><a href=<?= $profil_page_path?>>Profil</a></li>
+            </ul>
+        </nav>
+    </header>
--- public/pages/back-office/profil/profil.php
+++ public/pages/back-office/profil/profil.php
@@ -2,6 +2,16 @@
 
 <?php $css_path = "../../../css/index.css"?>
 
+<?php $desktop_logo_path = "../../../images/logo.png"?>
+<?php $tablet_logo_path = "../../../images/logo-tablet.png"?>
+
+<?php $collectes_page_path = "../collectes/collectes.php"?>
+<?php $blog_page_path = "../blog/blog.php"?>
+<?php $partenaires_page_path = "../partenaires/partenaires.php"?>
+<?php $evenements_page_path = "../evenements/evenements.php"?>
+<?php $utilisateurs_page_path = "../utilisateurs/utilisateurs.php"?>
+<?php $profil_page_path = "../profil/profil.php"?>
+
 <?php $facebook_logo_path = "../../../images/facebook-desktop-logo.png"?>
 <?php $instagram_logo_path = "../../../images/instagram-desktop-logo.png"?>
 
