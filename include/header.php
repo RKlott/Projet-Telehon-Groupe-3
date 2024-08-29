8f19c1913c195ad835fcac62035179bec1a1ce0c
patrick <patrack17@gmail.com>
2024-08-29 14:51:02 +0200

    modif scss

--- include/header.php
+++ include/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,12 +9,13 @@
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
+
 <body>
-<header class="">
-        <img id="logo-desktop" src=<?= $desktop_logo_path?> class="" alt="logo du site">
-        
-<!--//TODO Importer un logo de la version tablette avec les bonnes dimensions (ce logo là est mal dimensionné)-->
-        <img id="logo-tablet" src=<?= $tablet_logo_path?> class="" alt="logo du site en version tablette">
+    <header class="">
+        <img id="logo-desktop" src=<?= $desktop_logo_path ?> class="" alt="logo du site">
+
+        <!--//TODO Importer un logo de la version tablette avec les bonnes dimensions (ce logo là est mal dimensionné)-->
+        <img id="logo-tablet" src=<?= $tablet_logo_path ?> class="" alt="logo du site en version tablette">
         <nav class="">
             <ul class="">
                 <li class="frontoffice_navbar_link"><a href=<?= $accueil_page_path?>>Accueil</a></li>
