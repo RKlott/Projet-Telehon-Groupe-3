8b9df5a37a599ba16021858ad2957d0de2765a78
antoine <antoine.rigole@live.fr>
2024-08-29 16:29:53 +0200

    avancement de la page evenements et responsive du compteur

--- public/pages/front-office/evenements/evenements.php
+++ public/pages/front-office/evenements/evenements.php
@@ -11,6 +11,11 @@
 <?php  require '../../../../include/header.php'; ?>
 
 <main>
+    <div class="event_wrapper_top">
+
+    </div>
+
+    <!---------- COMPTEUR -------------->
     <div class="compteur">
         <div class="chiffres_compteur">
             <div class="chiffre_compteur">
@@ -34,6 +39,172 @@
         </div>
     </div>
 
+    <!------- SECTION EVENEMENTS ------->
+<section class="event_evenements">
+    <h2 class="event_titre">Evenements</h2>
+
+    <!-- CARTE 1 -->
+
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/repas_dansant_mini.webp" alt="un repas dansant">
+        </div>
+        <div class="event_contenu_card">
+            <div class="event_test">
+            <h3 class="event_titre_contenu_card">Repas dansant</h3>
+            <p class="event_paragraph_contenu_card">Profitez d'une soirée conviviale avec un délicieux repas suivi d'une ambiance festive sur la piste de danse. Venez vous amuser tout en soutenant la cause du Téléthon !</p>
+            </div>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- CARTE 2 -->
+             
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/boutique_mini.webp" alt="des peluches telethon">
+        </div>
+        <div class="event_contenu_card">
+            <h3 class="event_titre_contenu_card">Boutique Téléthon</h3>
+            <p class="event_paragraph_contenu_card">Faites vos achats solidaires à la Boutique Téléthon ! Vous y trouverez des objets artisanaux, des souvenirs et bien plus encore. Chaque achat contribue directement à la collecte de fonds pour le Téléthon.</p>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- CARTE 3 -->
+             
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/collecte_cartons_mini.webp" alt="une collecte de cartons">
+        </div>
+        <div class="event_contenu_card">
+            <h3 class="event_titre_contenu_card">Récupération de cartons</h3>
+            <p class="event_paragraph_contenu_card">Participez à une action écologique et solidaire en apportant vos cartons à notre point de collecte. Tous les cartons récupérés seront recyclés, et les fonds générés seront reversés au Téléthon.</p>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- CARTE 4 -->
+             
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/concert_mini.webp" alt="un concert">
+        </div>
+        <div class="event_contenu_card">
+            <h3 class="event_titre_contenu_card">Concerts</h3>
+            <p class="event_paragraph_contenu_card">Assistez à une série de concerts exceptionnels mettant en vedette des artistes locaux. Vivez une expérience musicale inoubliable tout en contribuant à une belle cause !</p>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- CARTE 5 -->
+             
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/bal_country_mini.webp" alt="un bal country">
+        </div>
+        <div class="event_contenu_card">
+            <h3 class="event_titre_contenu_card">Bals country</h3>
+            <p class="event_paragraph_contenu_card">Enfilez vos bottes et votre chapeau pour une soirée country inoubliable ! Initiez-vous aux danses western ou montrez vos talents sur la piste tout en participant à la collecte de fonds pour le Téléthon.</p>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- CARTE 6 -->
+             
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/tournoi_belotte_mini.webp" alt="des cartes dans une main">
+        </div>
+        <div class="event_contenu_card">
+            <h3 class="event_titre_contenu_card">Tournois de belotte</h3>
+            <p class="event_paragraph_contenu_card">Rejoignez-nous pour des tournois de belote passionnants ! Que vous soyez débutant ou expert, venez passer un bon moment et tenter de remporter des prix, tout en soutenant le Téléthon.</p>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- CARTE 7 -->
+             
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/evenements_sportifs_mini.webp" alt="un evenement sportif">
+        </div>
+        <div class="event_contenu_card">
+            <h3 class="event_titre_contenu_card">Evenements sportifs</h3>
+            <p class="event_paragraph_contenu_card">Participez à des compétitions et démonstrations sportives pour tous les âges et niveaux. Une belle occasion de se dépenser tout en contribuant à une cause solidaire.</p>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- CARTE 8 -->
+             
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/marches_mini.webp" alt="un petit marché">
+        </div>
+        <div class="event_contenu_card">
+            <h3 class="event_titre_contenu_card">Marchés</h3>
+            <p class="event_paragraph_contenu_card">Flânez dans nos marchés solidaires où vous trouverez une variété de produits locaux, artisanaux et gourmands. Les bénéfices des ventes seront intégralement reversés au Téléthon.</p>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- CARTE 9 -->
+             
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/lotos_mini.webp" alt="des cartons et jetons de loto">
+        </div>
+        <div class="event_contenu_card">
+            <h3 class="event_titre_contenu_card">Lotos</h3>
+            <p class="event_paragraph_contenu_card">Testez votre chance lors de nos lotos organisés pour le Téléthon ! De nombreux lots à gagner dans une ambiance chaleureuse et conviviale.</p>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- CARTE 10 -->
+             
+    <div class="event_card">
+        <div class="event_photo">
+            <img src="../../../images/course_en_equipe_mini.webp" alt="des personnes qui courent">
+        </div>
+        <div class="event_contenu_card">
+            <h3 class="event_titre_contenu_card">Courses en équipe</h3>
+            <p class="event_paragraph_contenu_card">Formez une équipe et participez à nos courses conviviales et ludiques. Une excellente occasion de se dépasser ensemble pour la bonne cause !</p>
+            <div class="event_cta_date">
+                <a class="event_btn_cta_card" href="http://">Voir les détails</a>
+                <p class="event_date_card">6 septembre 2024 à 19h00</p>
+            </div>
+        </div>
+    </div>
+</section>
 </main>
 
 
